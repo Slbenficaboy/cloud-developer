3a0a8707902f537205b5cc0b86e153284e2f89a9
Eduardo Cerqueira <eduardo.l.cerqueira@lmco.com>
2021-06-12 23:26:23 -0400

    Implement Course 4, lesson 1.

--- course-04/exercises/lesson-1/starter-code/index.js
+++ course-04/exercises/lesson-1/starter-code/index.js
@@ -11,32 +11,55 @@ const cloudwatch = new AWS.CloudWatch();
 
 exports.handler = async (event) => {
   // TODO: Use these variables to record metric values
+  console.log("Start execution now...")
   let endTime
-  let requestWasSuccessful
+  let requestWasSuccessful = 1
 
   const startTime = timeInMs()
-  await axios.get(url)
-
-  // Example of how to write a single data point
-  // await cloudwatch.putMetricData({
-  //   MetricData: [
-  //     {
-  //       MetricName: 'MetricName', // Use different metric names for different values, e.g. 'Latency' and 'Successful'
-  //       Dimensions: [
-  //         {
-  //           Name: 'ServiceName',
-  //           Value: serviceName
-  //         }
-  //       ],
-  //       Unit: '', // 'Count' or 'Milliseconds'
-  //       Value: 0 // Total value
-  //     }
-  //   ],
-  //   Namespace: 'Udacity/Serveless'
-  // }).promise()
-
+  try {
+    await axios.get(url)
+  } catch (error) {
+    requestWasSuccessful = 0
+  }
+  endTime = timeInMs()
+  let totalTime = endTime - startTime
+  
+  console.log("Apply Metrics")
   // TODO: Record time it took to get a response
+  await cloudwatch.putMetricData({
+    MetricData: [
+      {
+        MetricName: 'Latency', // Use different metric names for different values, e.g. 'Latency' and 'Successful'
+        Dimensions: [
+          {
+            Name: 'ServiceName',
+            Value: serviceName
+          }
+        ],
+        Unit: 'Milliseconds', // 'Count' or 'Milliseconds'
+        Value: totalTime // Total value
+      }
+    ],
+    Namespace: 'Udacity/Serveless'
+  }).promise()
+
   // TODO: Record if a response was successful or not
+  await cloudwatch.putMetricData({
+    MetricData: [
+      {
+        MetricName: 'Successful', // Use different metric names for different values, e.g. 'Latency' and 'Successful'
+        Dimensions: [
+          {
+            Name: 'ServiceName',
+            Value: serviceName
+          }
+        ],
+        Unit: 'Count', // 'Count' or 'Milliseconds'
+        Value: requestWasSuccessful // Total value
+      }
+    ],
+    Namespace: 'Udacity/Serveless'
+  }).promise()
 }
 
 function timeInMs() {
